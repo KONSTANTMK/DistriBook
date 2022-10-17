960b533175c2d004cfe41cae8086aba9ca5718a9
konstantmk <konstantmk@gmail.com>
2022-10-17 22:47:14 +0600

    Update project files

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,138 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CategoryModel> objCategoryList = _db.Categories.ToList();
+                
+            return View(objCategoryList);
+        }
+
+        //CREATE/////////////////////////////////////////////////
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(CategoryModel obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name","Имя категории и порядковый номер не могут быть одинаковыми!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Категория успешно создана!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //EDIT/////////////////////////////////////////////////
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+
+            if (id==null||id==0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(CategoryModel obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Имя категории и порядковый номер не могут быть одинаковыми!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Категория успешно отредактирована!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //DELETE/////////////////////////////////////////////////
+
+        //GET
+       
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Категория успешно удалена!";
+            return RedirectToAction("Index");
+           
+          
+        }
+    }
+
+}
---
+++ BulkyBookWeb/Models/CategoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Имя")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Порядковый номер")]
+        [Range(1,100,ErrorMessage="Введите число от 1 до 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
