b38fc9a381081a753d469f2ab6dc90dabcbb64c0
konstantmk <konstantmk@gmail.com>
2022-10-20 16:47:45 +0600

    Create product model

---
+++ BulkyBook.Models/ProductModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public string? ISBN { get; set; }
+        [Required]
+        public string? Author { get; set; }
+        [Required]
+        [Range(1,10000)]
+        public int ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int Price { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int Price50 { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public int Price100 { get; set; }
+        public string? ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public CategoryModel? Category { get; set; }
+        public int CoverTypeId { get; set; }
+        public CoverTypeModel? CoverType { get; set; }
+    }
+}
