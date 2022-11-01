fc416b24e7e93e14c9200708b9f91dade89a95b5
konstantmk <konstantmk@gmail.com>
2022-11-01 18:43:10 +0600

    Create ShoppingCartRepository

---
+++ BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCartModel>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public ShoppingCartRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(ShoppingCartModel obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
