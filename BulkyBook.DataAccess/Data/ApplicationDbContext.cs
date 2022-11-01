f4c4e6af08b39e9ac975ef2c0de72217532b41e3
konstantmk <konstantmk@gmail.com>
2022-11-02 01:16:27 +0600

    Add-migration OrderHeader and OrderDetail to database

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -18,4 +18,6 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<ApplicationUserModel> ApplicationUsers { get; set; }
     public DbSet<CompanyModel> Companies { get; set; }
     public DbSet<ShoppingCartModel> ShoppingCarts { get; set; }
+    public DbSet<OrderHeaderModel> OrderHeaders { get; set; }
+    public DbSet<OrderDetailModel> OrderDetail { get; set; }
 }
