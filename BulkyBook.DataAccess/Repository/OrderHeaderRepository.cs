c9024b2421ff3fb26cd6c0c03eefb6f3f4b9c8e5
konstantmk <konstantmk@gmail.com>
2022-11-02 15:10:45 +0600

    Add OrderDetail and OrderHeader repositories

---
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeaderModel>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public OrderHeaderRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(OrderHeaderModel obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus=orderStatus;
+                if(paymentStatus != null)
+                {
+                    orderFromDb.PaymentStatus=paymentStatus;
+                }
+            }
+        }
+    }
+}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -22,6 +22,8 @@ namespace BulkyBook.DataAccess.Repository
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public ICategoryRepository Category {get;private set;}
@@ -29,7 +31,9 @@ namespace BulkyBook.DataAccess.Repository
         public IProductRepository Product { get;private set;}
         public ICompanyRepository Company {get;private set;}
         public IShoppingCartRepository ShoppingCart {get;private set;}
-        public IApplicationUserRepository ApplicationUser { get; set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
         public void Save()
         {
