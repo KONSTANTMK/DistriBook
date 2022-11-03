b766816cb61d1dc80ef574896164d63006aa1adc
konstantmk <konstantmk@gmail.com>
2022-11-03 17:28:12 +0600

    Email sender

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using Org.BouncyCastle.Tls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,21 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("konstantmk@gmail.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text=htmlMessage};
+
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com",587,MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("konstantmk@gmail.com", "gzguywjtmyjmxcyd");
+               // gzguywjtmyjmxcyd
+
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
