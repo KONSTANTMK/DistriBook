f8d1a3efb69bcd3ae6b2f1f8e1b46d6b6eaec9e7
konstantmk <konstantmk@gmail.com>
2022-11-03 15:34:04 +0600

    Access changes

--- BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,15 @@
 ﻿using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles =SD.Role_Admin)]
     public class CategoryController : Controller
     {
 
