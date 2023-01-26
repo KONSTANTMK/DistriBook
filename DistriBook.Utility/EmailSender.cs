060740ed2122eda29f4df5e4a230fc1abbc9e17f
konstantmk <konstantmk@gmail.com>
2023-01-26 15:49:41 +0600

    Final changes

--- DistriBook.Utility/EmailSender.cs
+++ DistriBook.Utility/EmailSender.cs
@@ -15,15 +15,15 @@ namespace DistriBook.Utility
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("konstantmk@gmail.com"));
+            emailToSend.From.Add(MailboxAddress.Parse("konstantmk@mail.ru"));
             emailToSend.To.Add(MailboxAddress.Parse(email));
             emailToSend.Subject = subject;
             emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text=htmlMessage};
 
             using (var emailClient = new SmtpClient())
             {
-                emailClient.Connect("smtp.gmail.com",587,MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("konstantmk@gmail.com", "gzguywjtmyjmxcyd");
+                emailClient.Connect("smtp.mail.ru",587,MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("konstantmk@mail.ru", "PAYbzEbWbc6CbBjTgwRn");
                // gzguywjtmyjmxcyd
 
                 emailClient.Send(emailToSend);
